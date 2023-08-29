33add6f1cb9ce2d462e694e06d901de20347c97b
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2023-08-29 11:59:25 +0100

    refactor Calendar & CalendarGrid into full React components

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -6,7 +6,7 @@ import { add_days } from '../Utility/Dates/Dates'
 
 const Calendar = props => {
 
-   const num_days = 168
+   const num_days = 168    // c.6 months
 
    // get date of Sunday of the current week
    let current_week_start_date = new Date()
@@ -15,10 +15,12 @@ const Calendar = props => {
    // get date of Sunday of the grid start week (assumes -num_days is always taking us back to a Sunday)
    let grid_start_date = add_days(current_week_start_date,parseInt(-num_days))
 
+   // to do : we could ensure grid_start day is always a Sunday here..   use same ' - xxx.getDay() ' trick as above
+
+   // to do : test w/ different start_dates - eg middle of month etc..
+
    return (
-      <div>
-         <CalendarGrid start_date={grid_start_date} num_days={num_days} sessions={props.sessions} />
-      </div>
+      <CalendarGrid start_date={grid_start_date} num_days={num_days} sessions={props.sessions} />
    )
 }
 
--- src/components/Calendar/CalendarGrid/CalendarGrid.jsx
+++ src/components/Calendar/CalendarGrid/CalendarGrid.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import { get_day,get_month,extract_db_date_month } from '../../Utility/Dates/Dates'
+
 
 
 // CalendarGrid
@@ -7,56 +9,107 @@ import React from 'react'
 
 
 // to do : 
+// - align 'month' labels
 // - mouseover - tooltip details for session? 
 // - deal w/ user not ending a session
+// - verify 'props.num_days' is valid.
+
 
 
 const CalendarGrid = props => {
 
-   // to do : verify 'props.num_days' is valid.
+   const [grid,setGrid] = useState(Array(props.num_days + 2).fill(0))
 
-   const grid = Array(props.num_days + 2).fill(0)     // to do : tidy displaying days up to and including today..
-   let key_value = 0
-
-   console.log('calendar grid start date: ' , props.start_date)
-
-   const init_grid_array = () => {
-
+   useEffect(() => {
+   
       // calc offset of each session from 0 index date in the grid - 'start_date'
       let temp_date = null
 
       // calc grid offset for each session 
-      props.sessions.forEach(session => {
+      if(props.sessions) {
+         props.sessions.forEach(session => {
 
-         // to do : try alternative of just re-assigning date to same Date object here... quicker? (scope new Date outside this loop)
-         temp_date = new Date(session.started_at)         
+            // to do : try alternative of re-assigning date to same Date object here... quicker? (scope new Date outside this loop)
+            temp_date = new Date(session.started_at)         
 
-         // add grid offset to the Session - excluding dates before start_date
-         let offset = Math.round((temp_date - props.start_date ) / (1000 * 3600 * 24))
-         session.offset = offset > 0 ? offset : null
-      })
+            // add grid offset to the Session - excluding Sessions before start_date
+            let offset = Math.round((temp_date - props.start_date ) / (1000 * 3600 * 24))
+            session.offset = offset > 0 ? offset : null
+         })
 
-      // insert sessions into grid array
-      props.sessions.forEach(session => {
-         if(session.offset) {
-            grid[session.offset] = session
-         }
-      })
+         // insert sessions into grid array
+         let modified = grid
+         props.sessions.forEach(session => {
+            if(session.offset) {
+               modified[session.offset] = session
+
+            }
+         })
+         setGrid(modified)
+      }
+   },[props.session])
+
+
+   const month_labels = (start_date) => {
+
+      // we maintain grid alignment by using same <li> dimensions
+
+      // to do : calc spacing - calc index of array to insert label into..
+      //  since cells in the grid don't know their corresponding date, we can't check against them..
+      //  so try stepping backwards from known today's date (and it's position in current month) (or forwards from start_date?)
+  
+      let month_label_key = 0
+      let grid_months_labels =  Array(6)
+
+      const start_month_index = start_date.getMonth()
+      for(let i = start_month_index ; i < start_month_index + 6; i++) {
+         grid_months_labels[i] = get_month(i,1)
+      }
+
+      return (
+         <ul className="grid grid-flow-col text-xs text-gray-900">
+            <li key={month_label_key++} className="bg-blue-100 w-4 h-4 m-px "></li>
+            {grid_months_labels.map((label) => (
+               <React.Fragment key={month_label_key++}>
+                  <li key={month_label_key++} className="bg-blue-100 w-4 h-4 m-px ">{label}</li>
+                  <li key={month_label_key++} className="bg-blue-100 w-4 h-4 m-px "></li>
+                  <li key={month_label_key++} className="bg-blue-100 w-4 h-4 m-px "></li>
+                  <li key={month_label_key++} className="bg-blue-100 w-4 h-4 m-px "></li>
+               </React.Fragment>
+            ))}
+            <li key={month_label_key++} className="bg-blue-100 w-4 h-4 m-px "></li>
+         </ul>
+      )
    }
 
-   init_grid_array()
+   const day_labels = () => {
+      let day_label_key = 0
+      return (
+         <React.Fragment key={day_label_key++}>
+            <li key={day_label_key++} className=" w-4 h-4 rounded">s</li>
+            <li key={day_label_key++} className=" w-4 h-4 rounded">m</li>
+            <li key={day_label_key++} className=" w-4 h-4 rounded">t</li>
+            <li key={day_label_key++} className=" w-4 h-4 rounded">w</li>
+            <li key={day_label_key++} className=" w-4 h-4 rounded">t</li>
+            <li key={day_label_key++} className=" w-4 h-4 rounded">f</li>
+            <li key={day_label_key++} className=" w-4 h-4 rounded">s</li>
+         </React.Fragment>
+      )
+   }
 
-   // we inc. 'grid-template-rows' - tailwind's max is 'grid-rows-6'
-
+   // we inc. 'grid-template-rows' inline - tailwind's max is 'grid-rows-6'
    return (
-      <ul className="grid grid-flow-col " style={{gridTemplateRows:'repeat(7,1fr)'}}>
-         {grid.map((slot) => (
-            <li 
-               key={key_value++} 
-               className={`m-px w-4 h-4 rounded ${slot.id ? ' bg-gray-200 ' : ' bg-gray-100 '}`}
-               ></li>
-         ))}
-      </ul>
+      <>
+         {month_labels(props.start_date)}
+         <ul className="grid grid-flow-col text-xs text-gray-400 text-center" style={{gridTemplateRows:'repeat(7,1fr)'}}>
+            {day_labels()}
+            {grid.map((slot,index) => (
+               <li key={index} className={`m-px w-4 h-4 rounded ${slot.id ? ' bg-gray-300 ' : ' bg-gray-100 '}`}>
+                  {slot.started_at ? extract_db_date_month(slot.started_at) : ''}
+               </li>
+            ))}
+         </ul>
+      </>
    )
 }
 
