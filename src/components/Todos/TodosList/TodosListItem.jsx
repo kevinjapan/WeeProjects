713ad5fbd03dcea38ffab33ea7035e7b99a3295b
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2023-10-07 11:41:50 +0100

    Tidy code TodosListItem

--- src/components/Todos/TodosList/TodosListItem.jsx
+++ src/components/Todos/TodosList/TodosListItem.jsx
@@ -21,35 +21,33 @@ const TodosListItem = props => {
    if(parseInt(props.selected_todo_id) === todo.id) item_classes += ' border-blue-900'
    
    return (
-      <>
-         <li key={props.todo.id} className={`${item_classes} ${todo.on_going && !checked > 0 ? 'bg-yellow-200' : ''}`} >
-            <div className="flex justify-between gap-1 items-center w-full m-0">
+      <li key={props.todo.id} className={`${item_classes} ${todo.on_going && !checked > 0 ? 'bg-yellow-200' : ''}`} >
+         <div className="flex justify-between gap-1 items-center w-full m-0">
 
-               {todo.pin 
-                  ?  <BookmarkIcon style={{width:'16px',height:'16px',opacity:'.7'}}/> 
-                  :  <BookmarkIcon style={{width:'16px',height:'16px',opacity:'.3'}}/> 
-               }
-            
-               <div className="w-full">
-                  <div className={`${title_classes} ${checked ? 'line-through text-zinc-400 hover:text-zinc-600' : ''} `} 
-                     onClick={() => view_todo_details()}>{todo.title}</div>
-               </div>
-
-               {/* 
-               // open in separate window..
-                <div>
-                  <Link to={`/projects/${props.project_slug}/${props.task_slug}/${todo.slug}`}>
-                     <StyledButton aria-label="Open todo.">
-                        <BookOpenIcon style={{width:'12px',height:'12px'}}/>View
-                     </StyledButton>
-                  </Link>
-               </div>
-               */}
-
+            {todo.pin 
+               ?  <BookmarkIcon style={{width:'16px',height:'16px',opacity:'.7'}}/> 
+               :  <BookmarkIcon style={{width:'16px',height:'16px',opacity:'.3'}}/> 
+            }
+         
+            <div className="w-full">
+               <div className={`${title_classes} ${checked ? 'line-through text-zinc-400 hover:text-zinc-600' : ''} `} 
+                  onClick={() => view_todo_details()}>{todo.title}</div>
             </div>
-               
-         </li>
-      </>
+
+            {/* 
+            // open in separate window..
+               <div>
+               <Link to={`/projects/${props.project_slug}/${props.task_slug}/${todo.slug}`}>
+                  <StyledButton aria-label="Open todo.">
+                     <BookOpenIcon style={{width:'12px',height:'12px'}}/>View
+                  </StyledButton>
+               </Link>
+            </div>
+            */}
+
+         </div>
+            
+      </li>
    )
 }
 
