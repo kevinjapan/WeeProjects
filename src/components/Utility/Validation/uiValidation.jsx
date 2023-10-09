f37d5c6a2ba8e95b104a93e4132f1e4a4bcd174e
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2023-10-09 13:12:07 +0100

    Password Validation

--- src/components/Utility/Validation/uiValidation.jsx
+++ src/components/Utility/Validation/uiValidation.jsx
@@ -1,5 +1,9 @@
+import { LENGTHS as LEN } from '../../Utility/utilities/enums'
 
 
+//
+// String
+//
 export const validate_string = (str,tests,set_msg_callback,required=true) => {
 
    if(required && ((!str) || (str.length === 0))) {
@@ -24,6 +28,9 @@ export const validate_string = (str,tests,set_msg_callback,required=true) => {
 }
 
 
+//
+// Integer
+//
 export const validate_int = (value,tests,set_msg_callback) => {
 
     if((!value) || (isNaN(value))) {
@@ -49,7 +56,7 @@ export const validate_int = (value,tests,set_msg_callback) => {
 
 
 //
-// validate datetime_string
+// DateTime String
 // expects eg '2023-09-02 12:27:20'
 //
 export const validate_datetime_string = (value,tests,set_msg_callback) => {
@@ -61,3 +68,75 @@ export const validate_datetime_string = (value,tests,set_msg_callback) => {
    }
    return true
 }
+
+
+// 
+// Password
+// validate with confirmation password
+//
+export const validate_confirm_password = (password,password_confirmation,tests,set_msg_callback,required=true) => {
+   if(password !== password_confirmation) {
+      set_msg_callback('The password and password confirmation must match.')
+      return false;
+   }
+   return validate_password(password,tests,set_msg_callback,required)
+}
+
+
+// 
+// Password
+// we set 'tests' here in a single place
+//
+export const validate_password = (password,tests,set_msg_callback,required=true) => {
+   
+   // test for key - not value - since 0 would be falsey
+   let min_len = Object.hasOwn(tests,'min_length') ? tests['min_length'] : LEN.PASSWORD_MIN;
+   let max_len = Object.hasOwn(tests,'max_length') ? tests['max_length'] : LEN.PASSWORD_MAX;
+   
+   if(required && ((!password) || (password.length < min_len))) {
+      set_msg_callback('This field is required.')
+      return false
+   }
+
+   if(password.length < min_len) {
+      set_msg_callback('The entered value is too short. ' + min_len)
+      return false
+   }
+   if(password.length > max_len) {
+      set_msg_callback('The entered value is too long (' + password.length + '/' + max_len + ')')
+      return false
+   }
+
+   // to do : what char combination is required in our password ?
+
+   return true
+}
+
+
+//
+// Email
+// to do : adapt following from copied validate_string
+//
+export const validate_email = (str,tests,set_msg_callback,required=true) => {
+
+   if(required && ((!str) || (str.length === 0))) {
+      set_msg_callback('This field is required.')
+      return false
+   }
+
+   // perform client-configured tests
+   if(tests['min_length']) {
+      if(str.length < tests['min_length']) {
+         set_msg_callback('The entered value is too short.')
+         return false
+      }
+   }
+   if(tests['max_length']) {
+      if(str.length > tests['max_length']) {
+         set_msg_callback('The entered value is too long (' + str.length + '/' + tests['max_length'] + ')')
+         return false
+      }
+   }
+   return true
+}
+
