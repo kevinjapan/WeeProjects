10463b7005a4662f15a4f2b11567bdec268062c2
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2023-10-12 11:49:39 +0100

    Add click to close status msg dlg

--- src/components/App/AppStatus/AppStatus.jsx
+++ src/components/App/AppStatus/AppStatus.jsx
@@ -8,31 +8,46 @@ import { AppContext } from '../AppContext/AppContext'
 
 const AppStatus = props => {
 
-    // app-wide status msg
-    const {status_msg,setStatusMsg} = useContext(AppContext)
+   // app-wide status msg
+   const {status_msg,setStatusMsg} = useContext(AppContext)
+   const [is_open,setIsOpen] = useState(true)
 
-    // we create a local copy to linger in the DOM long enough for css transition to affect the text too
-    const [lingering_status_msg,setLingeringStatusMsg] = useState("")
+   // we create a local copy to linger in the DOM long enough for css transition to affect the text too
+   const [lingering_status_msg,setLingeringStatusMsg] = useState("")
 
-    useEffect(() => {
-        // eslint-disable-next-line
-        let clear_message,clear_lingering_message // prev. comment prevents warning ("clear_message not used.."")
-        if(status_msg !== "") {
-            setLingeringStatusMsg(status_msg)
-            clear_message = setTimeout(() => setStatusMsg(""),4000)
-            clear_lingering_message = setTimeout(() => setLingeringStatusMsg(""),5000)
-        }
-        return (clear_message) => {
-            clearTimeout(clear_message)
-            clearTimeout(clear_lingering_message)
-        }
-    },[status_msg,setStatusMsg,setLingeringStatusMsg])
 
-    return (
-        <div className={`app_status h-fit text-left p-8 border border-slate-300 shadow-lg rounded-xl bg-white  ${status_msg !== "" ? 'app_status_bg' : ''}`}>
-            {lingering_status_msg}
-        </div>
-    )
+   useEffect(() => {
+      setIsOpen(true)
+      // eslint-disable-next-line
+      let clear_message,clear_lingering_message // prev. comment prevents warning ("clear_message not used.."")
+      if(status_msg !== "") {
+         setLingeringStatusMsg(status_msg)
+         clear_message = setTimeout(() => setStatusMsg(""),4000)
+         clear_lingering_message = setTimeout(() => setLingeringStatusMsg(""),5000)
+      }
+      return (clear_message) => {
+         clearTimeout(clear_message)
+         clearTimeout(clear_lingering_message)
+      }
+   },[status_msg,setStatusMsg,setLingeringStatusMsg])
+
+
+   const close = () => {
+      setIsOpen(false)
+   }
+
+   const classes = 'app_status h-fit text-left p-8 border border-slate-300 shadow-lg rounded-xl bg-white'
+
+   return (
+      <>
+         {is_open
+            ?  <div className={`${classes} ${status_msg !== "" ? 'app_status_bg' : ''}`} onClick={() => close()}>
+                  {lingering_status_msg}
+               </div>
+            :  null
+         }
+      </>
+   )
 }
 
 
